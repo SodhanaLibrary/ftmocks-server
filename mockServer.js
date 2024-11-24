e740b4e3704354dc533f4c451188ee6977ba034e
Srinivas Dasari <sodhanaware@gmail.com>
2024-11-24 13:26:20 +0530

    Exaple project updates

--- mockServer.js
+++ mockServer.js
@@ -10,58 +10,66 @@ const app = express();
 app.use(bodyParser.json());
 
 app.all("*", (req, res) => {
-  const mockDataObj = loadMockData();
-  const mockData = mockDataObj.mocks;
-  const testName = mockDataObj.testName;
-  const defaultMockData = getDefaultMockData();
-  
-  let served = false;
-  let matchedMocks = mockData?.filter(mock => {
-    if (mock.fileContent.waitForPrevious && !served) {
-      return false;
-    }
-    served = mock.fileContent.served;
-    return compareMockToRequest(mock, req);
-  });
-  let foundMock = matchedMocks.find(mock => !mock.fileContent.served) ? matchedMocks.find(mock => !mock.fileContent.served) : matchedMocks[0];
-  
-  if(!foundMock) {
-    foundMock = defaultMockData?.find(mock => {
-      return compareMockToRequest(mock, req)
-    });
-  } else {
-    foundMock.fileContent.served = true;
-    const tetFilePath = path.join(process.env.MOCK_DIR, `test_${nameToFolder(testName)}`, `mock_${foundMock.id}.json`);
-    fs.writeFileSync(tetFilePath, JSON.stringify(foundMock.fileContent, null, 2));
-  }
-  // console.log(mockData, defaultMockData);
-  if (foundMock) {
-    const responseData = foundMock.fileContent;
-    setTimeout(() => {
-      const { content, headers, status } = responseData.response;
-      const includedHeaders = ["content-type"];
-
-      const headerKeys = Object.keys(headers);
-      headerKeys.forEach((aKey) => {
-        if (includedHeaders.includes(aKey)) {
-          res.set(aKey, headers[aKey]);
-        }
-      });
-
-      if(headers['content-type'] === 'image/png') {
-        var img = Buffer.from(content, 'base64');
-        res.status(status).end(img);
-      } else {
-        // console.log(responseData, response.content);
-        if(content) {
-          res.status(status).json(JSON.parse(content));
-        } else {
-          res.status(status).end('');
-        }
+  try {
+    const mockDataObj = loadMockData();
+    const mockData = mockDataObj.mocks;
+    const testName = mockDataObj.testName;
+    const defaultMockData = getDefaultMockData();
+    
+    let served = false;
+    let matchedMocks = mockData?.filter(mock => {
+      if (mock.fileContent.waitForPrevious && !served) {
+        return false;
       }
-    }, responseData.delay ? parseInt(responseData.delay) : 0)
-  } else {
-    res.status(404).json({ error: 'Not found' });
+      served = mock.fileContent.served;
+      return compareMockToRequest(mock, req);
+    }) || [];
+    let foundMock = matchedMocks.find(mock => !mock.fileContent.served) ? matchedMocks.find(mock => !mock.fileContent.served) : matchedMocks[0];
+    
+    if(!foundMock) {
+      foundMock = defaultMockData?.find(mock => {
+        return compareMockToRequest(mock, req)
+      });
+    } else {
+      foundMock.fileContent.served = true;
+      const tetFilePath = path.join(process.env.MOCK_DIR, `test_${nameToFolder(testName)}`, `mock_${foundMock.id}.json`);
+      fs.writeFileSync(tetFilePath, JSON.stringify(foundMock.fileContent, null, 2));
+    }
+    // console.log(mockData, defaultMockData);
+    if (foundMock) {
+      const responseData = foundMock.fileContent;
+      setTimeout(() => {
+        const { content, headers, status } = responseData.response;
+        const includedHeaders = ["content-type"];
+
+        const headerKeys = Object.keys(headers);
+        headerKeys.forEach((aKey) => {
+          if (includedHeaders.includes(aKey)) {
+            res.set(aKey, headers[aKey]);
+          }
+        });
+
+        if(headers['content-type'] === 'image/png') {
+          var img = Buffer.from(content, 'base64');
+          res.status(status).end(img);
+        } else {
+          // console.log(responseData, response.content);
+          if(content) {
+            try {
+              res.status(status).json(JSON.parse(content));
+            } catch(e) {
+              res.status(status).text(content);
+            }
+          } else {
+            res.status(status).end('');
+          }
+        }
+      }, responseData.delay ? parseInt(responseData.delay) : 0)
+    } else {
+      res.status(404).json({ error: 'Not found' });
+    }
+  } catch(e) {
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
